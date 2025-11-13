c721aff4aaf3661d974a4363ede1253428ac5d44
MdRehanAli <rehanali21d@gmail.com>
2025-11-13 10:51:50 +0600

    Added users apis

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT;
 
@@ -36,6 +36,20 @@ async function run() {
 
         // All Api's here 
         // Users Api
+
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { id: new ObjectId(id) };
+            const result = await usersCollection.findOne(query)
+            res.send(result);
+        })
+
         app.post('/users', async (req, res) => {
             const newUser = req.body;
 
